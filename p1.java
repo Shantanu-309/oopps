345bcc0864cef30c6156f9550cb9c03a01e1f487
Shantanu-309 <shantanbrao@gmail.com>
2025-07-23 11:23:11 +0530

    Add files via upload

---
+++ p1.java
@@ -0,0 +1,35 @@
+package oops;
+
+public class p1 {
+	
+	/*   public static void main(String[] args) {
+	      
+	        int age = 25;
+	        float height = 5.9f;  
+	        char grade = 'A';     
+	         
+	        System.out.println("Age: " + age);
+	        System.out.println("Height: " + height);
+	        System.out.println("Grade: " + grade);
+	    }
+	*/
+	
+	
+	public static void main(String[] args) {
+	      
+		int x = 2;
+		 while(x<=5)
+		 {
+			 System.out.println("hello");
+			 x++;
+		 }
+		 
+		
+	}
+	
+	
+	
+	
+	
+			
+}
---
+++ p2.java
@@ -0,0 +1,12 @@
+package oops;
+
+public class p2 {
+	public static void main(String[] args) {
+    
+	  int i;
+	  for(i=0;i<5;i++) {
+		  System.out.println("Hello");
+	  }
+
+		}
+}
